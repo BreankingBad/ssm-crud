7cdbab2b0954831cae319ec1dbf2d15506c94094
mxm <fjp790308448@gmail.com>
2017-08-19 21:26:08 +0800

    测试批量插入employee表

--- src/main/java/com/breaking/crud/bean/Employee.java
+++ src/main/java/com/breaking/crud/bean/Employee.java
@@ -15,7 +15,21 @@ public class Employee {
     
     
 
-    public Department getDepartment() {
+    public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(Integer empId, String empName, String gender, String email, Integer dId) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.gender = gender;
+		this.email = email;
+		this.dId = dId;
+	}
+
+	public Department getDepartment() {
 		return department;
 	}
 
--- src/main/java/com/breaking/crud/test/MapperTest.java
+++ src/main/java/com/breaking/crud/test/MapperTest.java
@@ -1,5 +1,11 @@
 package com.breaking.crud.test;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+
+import java.util.Random;
+import java.util.UUID;
+
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +13,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.breaking.crud.bean.Department;
+import com.breaking.crud.bean.Employee;
 import com.breaking.crud.dao.DepartmentMapper;
+import com.breaking.crud.dao.EmployeeMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"classpath:applicationContext.xml"})
@@ -16,12 +24,25 @@ public class MapperTest {
 	@Autowired
 	DepartmentMapper departmentMapper;
 	
+	@Autowired
+	EmployeeMapper employeeMapper;
+	
+	@Autowired
+	SqlSession sqlSession;
+	
 	@Test
 	public void testCRUD() {
 		System.out.println(departmentMapper);
 		
-		departmentMapper.insertSelective(new Department(null, "研发部"));
+	/*	departmentMapper.insertSelective(new Department(null, "研发部"));
 		departmentMapper.insertSelective(new Department(null, "测试部"));
+		*/
+		
+		EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
+		for (int i = 0; i < 1000; i++) {
+			String empName = i +UUID.randomUUID().toString().substring(0,6);
+			employeeMapper.insertSelective(new Employee(null, empName, "M", empName+"breaking@163.com", 25));
+		}
 		
 	}
 
